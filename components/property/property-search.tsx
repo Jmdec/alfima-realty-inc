6bd68520e44f995e4067b04778a94344aa61fc04
InfinitechCorp <infinitechcorp.ph@gmail.com>
2026-08-26 11:18:21 +0800

    Update property search options to remove '+' signs

--- components/property/property-search.tsx
+++ components/property/property-search.tsx
@@ -523,10 +523,10 @@ export function PropertySearch({
             >
               <option value="">Any</option>
               <option value="0">Studio</option>
-              <option value="1">1+</option>
-              <option value="2">2+</option>
-              <option value="3">3+</option>
-              <option value="4">4+</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
               <option value="5">5+</option>
             </select>
           </div>
